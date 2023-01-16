ccd4c015487e821bcf8a03f8a82ca33e4ccb02c1
Mher Hovhannisyan <mh3r99@gmail.com>
2023-01-16 19:39:07 +0400

    charts with navbar

--- src/App/App.js
+++ src/App/App.js
@@ -100,6 +100,7 @@ const App = () => {
                 <Route path="/stacked" element={<Stacked />} />
               </Routes>
             </div>
+            <Footer />
           </div>
         </div>
       </BrowserRouter>
